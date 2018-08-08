8bace6c17bf4c1cf7cccbcb4a33e836042bb7fe1
Justin Wilson <justin@jwils.co>
2018-08-08 15:31:28 -0400

    Remove `BrandConverter` class and comment out to-be-implemented tests

--- spec/mge_wholesale/order_spec.rb
+++ spec/mge_wholesale/order_spec.rb
@@ -1,54 +1,54 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe MgeWholesale::Order do
+# describe MgeWholesale::Order do
 
-  let(:credentials) { { username: 'login', password: 'password' } }
-  let(:default_header) do
-    {
-      customer:       '12345',
-      purchase_order: '1000-300',
-      ffl:            '12-aa-bb-1234',
-      shipping: {
-        name:      'Joe',
-        address_1: '123 Cherry Lane',
-        address_2: '',
-        city:      'Sunnyville',
-        state:     'SC',
-        zip:       '12345'
-      }
-    }
-  end
+#   let(:credentials) { { username: 'login', password: 'password' } }
+#   let(:default_header) do
+#     {
+#       customer:       '12345',
+#       purchase_order: '1000-300',
+#       ffl:            '12-aa-bb-1234',
+#       shipping: {
+#         name:      'Joe',
+#         address_1: '123 Cherry Lane',
+#         address_2: '',
+#         city:      'Sunnyville',
+#         state:     'SC',
+#         zip:       '12345'
+#       }
+#     }
+#   end
 
-  before do
-    ftp = instance_double('Net::FTP', :passive= => true, :debug_mode= => true)
-    allow(ftp).to receive(:chdir).with('Test/toBHC') { true }
-    allow(ftp).to receive(:puttextfile) { '' }
-    allow(Net::FTP).to receive(:open).with('ftp.host.com', 'login', 'password') { |&block| block.call(ftp) }
-    allow(ftp).to receive(:close)
-  end
+#   before do
+#     ftp = instance_double('Net::FTP', :passive= => true, :debug_mode= => true)
+#     allow(ftp).to receive(:chdir).with('Test/toBHC') { true }
+#     allow(ftp).to receive(:puttextfile) { '' }
+#     allow(Net::FTP).to receive(:open).with('ftp.host.com', 'login', 'password') { |&block| block.call(ftp) }
+#     allow(ftp).to receive(:close)
+#   end
 
-  describe '#add_header' do
-    let(:order) { MgeWholesale::Order.new(credentials) }
+#   describe '#add_header' do
+#     let(:order) { MgeWholesale::Order.new(credentials) }
 
-    before do
-      order.add_header(default_header)
-    end
+#     before do
+#       order.add_header(default_header)
+#     end
 
-    it { expect(order.instance_variable_get(:@header)[:customer]).to eq('12345') }
-    it { expect(order.instance_variable_get(:@header)[:purchase_order]).to eq('1000-300') }
-    it { expect(order.instance_variable_get(:@header)[:shipping][:name]).to eq('Joe') }
-    it { expect(order.instance_variable_get(:@header)[:shipping][:address_2]).to eq(nil) }
-  end
+#     it { expect(order.instance_variable_get(:@header)[:customer]).to eq('12345') }
+#     it { expect(order.instance_variable_get(:@header)[:purchase_order]).to eq('1000-300') }
+#     it { expect(order.instance_variable_get(:@header)[:shipping][:name]).to eq('Joe') }
+#     it { expect(order.instance_variable_get(:@header)[:shipping][:address_2]).to eq(nil) }
+#   end
 
-  describe '#submit!' do
-    let(:order) { MgeWholesale::Order.new(credentials) }
+#   describe '#submit!' do
+#     let(:order) { MgeWholesale::Order.new(credentials) }
 
-    before do
-      order.add_header(default_header)
-      order.add_item(item_number: 'ABCD', quantity: 1, price: '9.99')
-    end
+#     before do
+#       order.add_header(default_header)
+#       order.add_item(item_number: 'ABCD', quantity: 1, price: '9.99')
+#     end
 
-    it { expect(order.submit!).to eq(true) }
-  end
+#     it { expect(order.submit!).to eq(true) }
+#   end
 
-end
+# end
