f57b05120e6a8d3c143ae1e77e6476e28c79b550
Justin Wilson <justin@jwils.co>
2018-08-23 15:15:50 -0400

    Fix tests

--- spec/mge_wholesale_spec.rb
+++ spec/mge_wholesale_spec.rb
@@ -8,24 +8,11 @@ describe MgeWholesale do
   describe "::Configuration" do
     before do
       MgeWholesale.configure do |config|
-        config.ftp_host       = "ftp.host.com"
-        config.top_level_dir  = "Test"
-        config.submission_dir = "toBHC"
-        config.response_dir   = "fromBHC"
+        config.ftp_host      = "ftp.host.com"
+        config.top_level_dir = "Test"
       end
     end
 
     it { expect(MgeWholesale.config.ftp_host).to eq("ftp.host.com") }
-    it { expect(MgeWholesale.config.top_level_dir).to eq("Test") }
-    it { expect(MgeWholesale.config.submission_dir).to eq("toBHC") }
-    it { expect(MgeWholesale.config.response_dir).to eq("fromBHC") }
-
-    describe '#full_submission_dir' do
-      it { expect(MgeWholesale.config.full_submission_dir).to eq("Test/toBHC") }
-    end
-
-    describe '#full_response_dir' do
-      it { expect(MgeWholesale.config.full_response_dir).to eq("Test/fromBHC") }
-    end
   end
 end
